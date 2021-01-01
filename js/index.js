b507ee81c77acdf8b990de3af5a13dacc0687269
esmayorga1 <mayorga.sebas100@hotmail.com>
2021-01-01 11:12:24 -0500

    Window conocer la ruta

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-console.log( "Primera linea \nSegunda Linea");
+// Dow windows - Conocer la ruta
+var ruta = window.location; // Da como resultado un objeto
+console.log(ruta);
+console.log(ruta.pathname);
