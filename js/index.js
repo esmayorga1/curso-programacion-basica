c102ed4abb8c78fec8bd35186ff5fb5f10e7e080
esmayorga1 <mayorga.sebas100@hotmail.com>
2021-01-01 09:30:31 -0500

    Sebastian Mayorga@INGQUERGEO MINGW64 ~

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-var nombre = prompt("Cual es tu nombre");
-alert("Tu nombre es "+ nombre);
+var x = 10;
+var y = 2.7
+// Convirtiendo numeros a entero
+var c = parseInt (10.2 * 2.7);
+document.write(c);
+
+// Definiendo en numero de decimales
+var c = (10.2 * 2.7).toFixed(1);
+document.write(c);
+
+// Convirtiendo numeros a flotantes
+var c = parseFloat("10.5");
+document.write(c);
