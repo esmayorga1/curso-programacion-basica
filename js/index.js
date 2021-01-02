91380d5f0bfc868fd30036614aabadf6216aada9
esmayorga1 <mayorga.sebas100@hotmail.com>
2021-01-02 06:14:33 -0500

    Eventos de teclas

--- js/index.js
+++ js/index.js
@@ -1,10 +1,33 @@
-var boton = document.getElementById('bt');
-boton.addEventListener('click', mensaje);
-
-
-function mensaje(){
-    var formulario1 = document.getElementById('formulario1');
-    var datoFormulario1 = parseFloat (formulario1.value);
-
-    alert('El valor del formulario es ' + datoFormulario1);
+// Eventos de teclas
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
 }
+// keyup cuando suelto una tecla
+ document.addEventListener('keyup', mensaje);
+
+ var suma = 100;
+
+ function mensaje(evento){
+     console.log(evento);
+     var desplazamiento = 50;    
+
+     if(evento.keyCode == teclas.UP)
+     {
+         suma = suma + desplazamiento;
+         console.log('tecla hacia arriba '+ suma); 
+         
+     }
+
+     else if (evento.keyCode ==teclas.DOWN)
+     {
+        suma = suma - desplazamiento;
+         console.log('tecla hacia abajo' + suma); 
+     } 
+ }
+
+ 
+
+ 
