91eb978d7725ab481274ba04f035618e8eb6a008
esmayorga1 <mayorga.sebas100@hotmail.com>
2021-01-02 16:11:55 -0500

    Arrays

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
-// Programacion orientada objetos clases
-class DragonBollZ{
-    constructor(nombrePersonaje, ataquePersonaje, vidaPersonaje){
-        this.nombrePersonaje = nombrePersonaje;
-        this.ataquePersonaje = ataquePersonaje;
-        this.vidaPersonaje = vidaPersonaje
-    }
+// Arrays
 
-    dimeTuNombre(){
-        alert(this.nombrePersonaje)
-    }
-}
+var mascotas = ['Perro', 'Gato', 12, 15.5];
+// Agregar otra mascota al final
+mascotas.push('Loro');
+//  Consular la mascota Perro
+console.log(mascotas[0]);
 
-var picoro = new DragonBollZ('Picoro', 52, 70);
-console.log(picoro);
-picoro.dimeTuNombre();
+// Array anidados
+var personajes =[ 
+    nombres = ['juan', 'Camila', 'Laura'],
+    edades = [12, 15, 20]
+ ]
+
+ // Array asociativos
+var personajes2 =[]
+personajes2["nombre"] = 'Pepe';
+personajes2['edad'] =  50;
+console.log(personajes2);
+console.log(personajes2.edad);
